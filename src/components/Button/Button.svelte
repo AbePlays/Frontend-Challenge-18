2e8ec3739643cefd753ccf29ce584726571832e6
Abe <abhi.rawat456@gmail.com>
2021-05-12 18:03:39 +0530

    feat: added button and layout components

---
+++ src/components/Button/Button.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let content: string = "default";
+  export let variant: "plain" | "filled" = "plain";
+  export let shape: "pill" | "normal" = "normal";
+</script>
+
+<button
+  class="
+    {variant === 'filled'
+    ? 'bg-cyan text-white px-6 py-2'
+    : 'text-grayish-violet'} 
+    {shape === 'pill' ? 'rounded-full' : 'rounded-md'}"
+>
+  {content}
+</button>
