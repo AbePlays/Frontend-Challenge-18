0a90297148d79cc56f7796584a69b34b2b140cff
Abe <abhi.rawat456@gmail.com>
2021-05-12 16:56:20 +0530

    feat: Added Poppins Font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: ["./src/**/*.svelte"],
-
   theme: {
-    
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {},
-  plugins: []
+  plugins: [],
 };
