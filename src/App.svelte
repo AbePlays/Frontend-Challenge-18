398af5f25b6025ede899631c8b51d231b7c17467
Abe <abhi.rawat456@gmail.com>
2021-05-12 22:29:03 +0530

    refactor: added layout in App

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
   import Tailwind from "./Tailwind.svelte";
   import Navbar from "./containers/Navbar";
+  import Layout from "./components/Layout";
 </script>
 
 <Tailwind />
-<Navbar />
+<Layout>
+  <Navbar />
+</Layout>
 
 <style>
 </style>
