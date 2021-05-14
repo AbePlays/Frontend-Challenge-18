30cfa27b04ecdfaf9f49d40e60c6fb28d40f432e
Abe <abhi.rawat456@gmail.com>
2021-05-14 23:31:46 +0530

    feat: added isValidLink function

---
+++ src/containers/ShortenLink/helper.ts
@@ -0,0 +1,5 @@
+export const isValidLink = (url: string): boolean => {
+  const re =
+    /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/;
+  return re.test(url.toLowerCase());
+};
