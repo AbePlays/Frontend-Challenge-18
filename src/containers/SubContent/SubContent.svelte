5121687c6b316b75d9cfccc113b92bf45a55ceda
Abe <abhi.rawat456@gmail.com>
2021-05-14 12:08:51 +0530

    feat: added footer

---
+++ src/containers/SubContent/SubContent.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Button from "../../components/Button";
+</script>
+
+<div class="bg-dark-violet text-center px-4 py-16 bg-desktop-pattern">
+  <h1 class="text-3xl md:text-4xl text-white font-bold mb-6">
+    Boost your links today
+  </h1>
+  <Button content="Get Started" shape="pill" variant="filled" />
+</div>
