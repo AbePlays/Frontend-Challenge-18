7623b312779c59fa8954ecadcb8b98135a8a73a7
Abe <abhi.rawat456@gmail.com>
2021-05-17 11:18:30 +0530

    fix: fixed layout

--- src/components/LinkBox/LinkBox.svelte
+++ src/components/LinkBox/LinkBox.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { slide } from "svelte/transition";
   import Button from "../Button";
   import { copyText } from "./helper";
 
@@ -20,21 +21,24 @@
 </script>
 
 <div
-  class="bg-white rounded-lg sm:flex sm:justify-between sm:items-center sm:gap-4"
+  transition:slide
+  class="bg-white rounded-lg sm:flex sm:justify-between sm:items-center sm:gap-4 p-4"
 >
-  <p class="text-very-dark-violet px-4 py-2 sm:flex-1">{url}</p>
+  <p class="text-very-dark-violet mb-4 sm:mb-0 sm:flex-1">{url}</p>
   <div
-    class="border-t border-light-gray p-4 pt-2 sm:flex sm:justify-end sm:items-center sm:gap-4 sm:pt-4 sm:flex-1"
+    class="border-t border-light-gray sm:border-none pt-4 sm:pt-0 sm:flex-1 sm:flex sm:justify-end sm:items-center sm:gap-4"
   >
-    <p class="text-cyan mb-2 sm:mb-0">{shortenedUrl}</p>
-    <div>
-      <Button
-        content={copyOperationCompleted ? "Copied!" : "Copy"}
-        variant="filled"
-        shape="normal"
-        twClass={copyOperationCompleted ? "bg-dark-violet" : ""}
-        {onPress}
-      />
-    </div>
+    <p class="text-cyan mb-2 sm:mb-0 sm:inline sm:break-all ">
+      {shortenedUrl}
+    </p>
+    <Button
+      content={copyOperationCompleted ? "Copied!" : "Copy"}
+      variant="filled"
+      shape="normal"
+      twClass="w-full sm:w-auto {copyOperationCompleted
+        ? 'bg-dark-violet'
+        : ''}"
+      {onPress}
+    />
   </div>
 </div>
